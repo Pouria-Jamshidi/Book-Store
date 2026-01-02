767b93fa0863a03eae6bbeccfbe0856a3b0064a6
Pouria-Jamshidi <the.eye.programming@gmail.com>
2026-01-02 13:57:46 +0330

    added new applications to project settings

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'accounts.apps.AccountsConfig',
+    'sales.apps.SalesConfig',
 
 ]
 

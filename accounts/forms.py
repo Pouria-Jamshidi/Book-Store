958df6807b96be772b9b7926e4b63d1ad5216f7c
Pouria-Jamshidi <the.eye.programming@gmail.com>
2026-01-25 17:59:24 +0330

    added sign up and sign in pages

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,43 +1,47 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 User = get_user_model()
 
-# class Register_form(forms.ModelForm):
-#     password = forms.CharField(max_length=20, widget=forms.PasswordInput(), required=True, label="گذرواژه")
-#     confirm_password = forms.CharField(max_length=20, widget=forms.PasswordInput(), required=True, label="تکرار گذرواژه")
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password', 'confirm_password']
-#
-#     def clean(self):
-#         data = super().clean()
-#         password = data.get('password')
-#         confirm_password = data.get('confirm_password')
-#
-#         validate_password(password)
-#         if password and confirm_password and password != confirm_password:
-#             raise ValidationError('گذرواژه با تکرار آن مطابقت ندارد.')
-#         return data
-#
-#     def save(self, commit=True):
-#         """
-#         overriding save method so it hashes our password before saving
-#         """
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data.get('password'))
-#         user.save()
-#         return user
-
-# → due to an error with password , the field was not validated
 class Register_form(UserCreationForm):
         class Meta:
                 model = User
                 fields = ['username', 'email', 'password1', 'password2']
+                labels = {
+                    'username' : 'نام کاربری',
+                    'email' : 'ایمیل',
+                    'password1' : 'گذرواژه',
+                    'password2' : 'تکرار گذرواژه',
+                }
+                widgets = {
+                    'username': forms.TextInput(attrs={'class': 'form-styling', 'autofocus': True, 'style':'text-align:left;'}), #autofocus enables curser on this field as the page starts
+                    'email': forms.EmailInput(attrs={'class': 'form-styling', 'style':'text-align:left;'}),
+                }
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=40, label='نام کاربری', widget=forms.Textarea(attrs={'class': 'form-control','rows': 1}))
-    password = forms.CharField(max_length=40,label='رمز عبور', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['password1'].widget.attrs['class'] = 'form-styling'
+            self.fields['password1'].widget.attrs['style'] = 'text-align:left;'
+            self.fields['password2'].widget.attrs['class'] = 'form-styling'
+            self.fields['password2'].widget.attrs['style'] = 'text-align:left;'
+
+class Login_form(AuthenticationForm):
+    username = forms.CharField(
+        max_length=20,
+        label='نام کاربری',
+        widget = forms.TextInput(attrs={'class':'bnazaninBold form-styling', 'autofocus': True, 'style':'text-align:left;','id':'username'}),#autofocus enables curser on this field as the page starts
+    )
+
+    password = forms.CharField(
+        label='گذرواژه',
+        strip=False, # does not remove the space at the end or begining of user input. passwords should be exact
+        widget=forms.PasswordInput(attrs={'class':'bnazaninBold form-styling', 'style':'text-align:left;','id':'password'}),
+    )
+
+    remember_me = forms.BooleanField(
+        required=False,
+        initial=False,
+        label="من را بخاطر داشته باش",
+        widget=forms.CheckboxInput(attrs={'id':'checkbox'}),
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,59 @@
 
 from django.shortcuts import render,redirect,get_object_or_404
+from django.conf import settings
 from django.views import View
 from django.contrib import messages
-from django.contrib.auth import authenticate,login,logout
-from accounts.forms import Register_form, LoginForm
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from accounts.forms import Register_form, Login_form
 
 
 class RegisterView(View):
     def get(self, request):
         form = Register_form()
-        return render(request, 'accounts/register.html', {'register_form': form})
+        return render(request, 'accounts/register.html', {'form': form})
 
     def post(self, request):
         form = Register_form(request.POST)
         if form.is_valid():
             new_user = form.save()
-            messages.success(request, f'اکانت شما به نام کاربری {new_user.username} ساخته شد!')
+            messages.success(request, 'اکانت شما به نام کاربری {} ساخته شد!'.format(new_user.username))
             return redirect('home')
-        return render(request, 'accounts/register.html', {'register_form': form})
+        return render(request, 'accounts/register.html', {'form': form})
 
-class LoginView(View):
-    def get(self, request):
-        form = LoginForm()
-        return render(request,'accounts/login.html', {'login_form': form})
+class Login_view(LoginView):
+    form_class = Login_form
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
 
-    def post(self, request):
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            user = authenticate(username=data.get('username'), password=data.get('password'))
-            if user:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.error(request,'کاربری با این مشخصات یافت نشد')
-        return render(request, 'accounts/login.html', {'login_form': form})
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.info(request, 'شما قبلاً وارد شده‌اید')
+            # check for next parameter
+            next_url = request.GET.get('next')
+            if next_url:
+                return redirect(next_url)
+
+            # fallback to default success_url
+            return redirect(self.get_success_url())
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+
+        #  ===================== Remember me ======================
+        remember_me = form.cleaned_data.get('remember_me')
+        if not remember_me:
+            self.request.session.set_expiry(0)
+        #  ================== End of Remember me ==================
+
+        user = form.get_user()
+        messages.success(self.request,'کاربر {} با موفقیت وارد شد'.format(user.username))
+        return super().form_valid(form)
+
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'کاربری با این مشخصات یافت نشد')
+        return super().form_invalid(form)
 
 
 def logout_view(request):

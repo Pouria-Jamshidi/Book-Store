d0f3d28b8963fb22c6dc07de083869c86a417f4d
Pouria-Jamshidi <the.eye.programming@gmail.com>
2026-02-01 01:25:44 +0330

    fixed book detail template and fixed home page so it is dynamic

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     credit = models.IntegerField(default=0, blank=True, verbose_name="کیف پول")
     wishlist = models.ManyToManyField('core.Book', blank=True, related_name='wishlist', verbose_name= 'فهرست خواسته ها')
 
+
     class Meta:
         verbose_name = 'کاربر'
         verbose_name_plural = 'کاربر'

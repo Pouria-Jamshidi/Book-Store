2cc0b370759175765a8caa6186c264435467fe31
Pouria-Jamshidi <the.eye.programming@gmail.com>
2026-02-05 11:03:13 +0330

    minor fix

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from core.views import home, NewBook, NewAuthorView, NewGenreView, book_detail, BookScoreView, NavbarView, home_genre
-from django.conf import settings
-from django.conf.urls.static import static
+
 urlpatterns = [
     path('', home, name='home'),
     path('genre/<int:genre_id>/', home_genre, name='home_genre'),
@@ -12,9 +11,3 @@ urlpatterns = [
     path('book/like/<int:book_id>/', BookScoreView.as_view(), name='book_score'),
     path('navbar/items/', NavbarView.as_view(), name='navbar_items'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root= settings.MEDIA_ROOT
-    )
